45e99a2deb4efe1c7f40452e89da5e7741c4e8e3
juliette <111221678+yanieznaio@users.noreply.github.com>
2023-05-27 06:54:07 +0000

    change on StateProvider

--- src/compenents/StateProvider.js
+++ src/compenents/StateProvider.js
@@ -3,6 +3,35 @@ import { createContext, useState } from "react";
 export const StateContext = createContext();
 
 const StateProvider = ({ children }) => {
+  const checkDefaultOccupied = () => {
+    const Pieces = [
+      {
+        name: "rook",
+        squares: ["a", "h"],
+      },
+      {
+        name: "knight",
+        squares: ["b", "g"],
+      },
+      {
+        name: "Bishop",
+        squares: ["c", "f"],
+      },
+      {
+        name: "queen",
+        squares: ["d"],
+      },
+      {
+        name: "king",
+        squares: ["e"],
+      },
+      {
+        name: "pawn",
+        squares: ["a", "b", "c", "d", "e", "f", "g", "h"],
+      },
+    ];
+  };
+
   const createCaseName = () => {
     var arr = [];
     const letters = "abcdefgh";
@@ -13,11 +42,15 @@ const StateProvider = ({ children }) => {
     }
     return arr;
   };
+
   const createObject = () => {
     const caseName = createCaseName();
     var object = [];
     for (let i = 0; i < 64; i++) {
-      object[i] = { name: caseName[i] };
+      object[i] = {
+        name: caseName[i],
+        occupied: [caseName[i][1] === 8 || 7 | 1 | 2 ? true : false],
+      };
     }
     return object;
   };
